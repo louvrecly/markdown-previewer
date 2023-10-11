b32142df0b8167bb6f6258f9ca0ba0baa52b13fa
Louvre Chow <louvrecly@gmail.com>
2023-10-11 18:49:26 -0400

    create ExpandableDiv from Div element

---
+++ src/components/elements/ExpandableDiv.tsx
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+import Div from './Div';
+
+export interface ExpandableDivProps {
+  $isExpanded?: boolean;
+}
+
+const ExpandableDiv = styled(Div)<ExpandableDivProps>`
+  height: 100%;
+  transition: max-height 0.3s ease;
+  max-height: ${(props) => (props.$isExpanded ? 500 : 0)}px;
+  overflow: hidden;
+`;
+
+export default ExpandableDiv;
