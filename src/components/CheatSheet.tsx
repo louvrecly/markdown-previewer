1c4845572d594a995de37bb6155f38bac7401030
Louvre Chow <louvrecly@gmail.com>
2023-10-11 18:54:25 -0400

    create CheatSheet component from Div element

---
+++ src/components/CheatSheet.tsx
@@ -0,0 +1,30 @@
+import Div from './elements/Div';
+
+const cheatSheetItems = [
+  '# H1',
+  '## H2',
+  '### H3',
+  '**bold**',
+  '*italic*',
+  '[Link](http://a.com)',
+  '![Image](http://url/a.png)',
+  '`inline code`',
+  '```block code```',
+  '- list item',
+];
+
+const CheatSheet = () => {
+  return (
+    <Div $padding="10px 30px 0">
+      <h2>Markdown Cheat Sheet</h2>
+
+      <ul>
+        {cheatSheetItems.map((item, idx) => (
+          <li key={idx}>{item}</li>
+        ))}
+      </ul>
+    </Div>
+  );
+};
+
+export default CheatSheet;
