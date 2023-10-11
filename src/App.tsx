2c9e91b7c1e753279ef2cc0f2dd9cc1fb614c89c
Louvre Chow <louvrecly@gmail.com>
2023-10-11 11:57:18 -0400

    wrap HeaderBar and Body with Layout component in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import Layout from './components/Layout';
 import HeaderBar from './components/HeaderBar';
 import Body from './components/Body';
 
 const App = () => {
   return (
-    <>
+    <Layout $direction="column" $alignItems="stretch">
       <HeaderBar title="Markdown Previewer" />
 
       <Body />
-    </>
+    </Layout>
   );
 };
 
